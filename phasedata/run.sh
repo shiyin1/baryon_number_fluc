8e15de50fbd736a3e92c1d2cd78918dadc5abab8
shi <shi.yin@theo.physik.uni-giessen.de>
2026-02-12 12:50:01 +0100

    data

---
+++ phasedata/run.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+cd mub0/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub50/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub100/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub150/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub200/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub250/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub300/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub350/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub400/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub450/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub500/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub550/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub600/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub650/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub700/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub750/final
+rm *.o
+rm exe
+make
+./exe
+cd ../../mub800/final
+rm *.o
+rm exe
+make
+./exe
